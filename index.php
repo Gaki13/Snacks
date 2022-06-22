63ddc9112efbe023d80801464fdd7d45f567dfff
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-06-22 12:48:53 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!-- Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
+<?php 
+    function getArrayOfIntegers($min, $max, $nItems)
+    {
+        $Array=[];
+
+        while(count($Array) > $nItems) {
+            $number = rand($min, $max);
+
+            if(!in_array($number, $Array)) {
+                $Array[] = $number;
+            }
+        }
+        return $Array;
+    }
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>snack 1</title>
+</head>
+<body>
+    <?php var_dump  getArrayOfIntegers(1, 100, 15) ?>
+</body>
+</html>
